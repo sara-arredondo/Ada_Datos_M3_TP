050d576fbde18011c84491cc731d473a1894a416
Sara Arredondo Giraldo <sara.arredondo.giraldo@gmail.com>
2025-02-23 11:51:15 -0500

    se agregan funcionalidades para guardar LS, Pintar datos, Filtrar por tipo de gasto y mostrar ganancias y gastos en el panel de balance

---
+++ funciones.js
@@ -0,0 +1,36 @@
+// ---------------------------------------------inicio funcion para leer Local Storage ---------------------------------------------------
+let datosTodasLasOperaciones = JSON.parse(localStorage.getItem("operaciones")) || []; 
+
+
+function leerLocalStorage(key) {
+    const datos = JSON.parse(localStorage.getItem(key));
+    datosTodasLasOperaciones = datos;
+    return datos ? datos : [];
+}
+
+function guardarLocalStorage(key, data) {
+    localStorage.setItem(key, JSON.stringify(data))
+}
+
+function agregarOperacion(objetoNuevaOperacion) {
+    datosTodasLasOperaciones.push(objetoNuevaOperacion)
+    guardarLocalStorage("operaciones", datosTodasLasOperaciones)
+}
+
+// ---------------------------------------------inicio funcion para exportar datos ---------------------------------------------------
+
+export default {
+    leerLocalStorage,
+    guardarLocalStorage,
+    agregarOperacion,
+    datosTodasLasOperaciones,
+}
+
+
+
+
+
+
+
+
+
